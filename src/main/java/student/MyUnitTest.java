6357c6ebba44f6459063df0012a8122952109c49
Keith Mannock <keith@dcs.bbk.ac.uk>
2017-06-19 12:14:54 +0100

    init

---
+++ src/main/java/student/MyUnitTest.java
@@ -0,0 +1,13 @@
+package student;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class MyUnitTest {
+
+  @Test
+  public void testMethod() {
+    //fail("Example test");
+  }
+}
